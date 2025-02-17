8ec2c35852c4ce3e81395958abf67d5ee8c9f327
O.Varushchyk <alexvarushik@gmail.com>
2025-02-17 13:08:44 +0200

    init commit

---
+++ faifly-app/src/main/java/com/task/faiflyapp/FaiflyAppApplication.java
@@ -0,0 +1,13 @@
+package com.task.faiflyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaiflyAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FaiflyAppApplication.class, args);
+  }
+
+}
