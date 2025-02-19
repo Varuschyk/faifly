9432f60accc8968110f91bbe1203dcdeeb37bae2
O.Varushchyk <alexvarushik@gmail.com>
2025-02-19 17:41:56 +0200

    Add visit controller

---
+++ faifly-api-core/src/main/java/com/task/faiflyapicore/controller/VisitControllerImpl.java
@@ -0,0 +1,45 @@
+package com.task.faiflyapicore.controller;
+
+import com.task.faiflyapicore.mapper.VisitMapper;
+import com.task.faiflyapicore.service.VisitService;
+import com.task.faiflywebapi.controller.VisitController;
+import com.task.faiflywebapi.dto.visit.VisitRequestDto;
+import com.task.faiflywebapi.dto.visit.VisitResponseDto;
+import com.task.faiflywebapi.dto.visit.VisitsResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+public class VisitControllerImpl implements VisitController {
+
+  private final VisitService visitService;
+  private final VisitMapper visitMapper;
+
+  @Override
+  @GetMapping("/v1/visit/{id}")
+  public ResponseEntity<VisitsResponse> getVisits(
+      @PathVariable("id") @NotNull final Long patientId) {
+    final var visitsReadPojo = visitService.getVisits(patientId);
+    final var visitsResponseDto = visitsReadPojo.stream()
+        .map(visitMapper::toVisitResponseDto).toList();
+    return ResponseEntity.ok(new VisitsResponse(visitsResponseDto));
+  }
+
+  @Override
+  @PostMapping("/v1/visit")
+  public VisitResponseDto createVisit(@Valid @NotNull @RequestBody final VisitRequestDto visitRequestDto) {
+    final var visitWritePojo = visitMapper.toVisitWritePojo(visitRequestDto);
+    final var createdVisit = visitService.createVisit(visitWritePojo);
+    return visitMapper.toVisitResponseDto(createdVisit);
+  }
+}
