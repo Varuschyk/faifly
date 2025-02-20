e61fbdbc6d7f24bd91ad61aec259d7d2c5490752
O.Varushchyk <alexvarushik@gmail.com>
2025-02-20 20:07:48 +0200

    Add new methods in visit repository

--- faifly-api-core/src/main/java/com/task/faiflyapicore/persistence/repository/VisitRepository.java
+++ faifly-api-core/src/main/java/com/task/faiflyapicore/persistence/repository/VisitRepository.java
@@ -2,12 +2,24 @@ package com.task.faiflyapicore.persistence.repository;
 
 import com.task.faiflyapicore.persistence.enity.VisitEntity;
 import jakarta.annotation.Nonnull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+import java.time.Instant;
+import java.util.Set;
 
 @Repository
 public interface VisitRepository extends JpaRepository<VisitEntity, Long> {
 
-  List<VisitEntity> findAllByPatientId(@Nonnull final Long patientId);
+  int countByDoctorFirstNameAndDoctorLastName(@Nonnull final String firstName,
+                                              @Nonnull final String lastName);
+
+  boolean existsByDoctorIdAndStartDateTimeGreaterThanEqualAndEndDateTimeLessThanEqual(@Nonnull final Long doctorId,
+                                                                                      @Nonnull final Instant startDateTime,
+                                                                                      @Nonnull final Instant endDateTime);
+
+  Page<VisitEntity> findAllByPatientFirstNameAndDoctorIdIn(@Nonnull final String firstName,
+                                                           @Nonnull final Set<Long> doctorIds,
+                                                           @Nonnull final Pageable pageable);
 }
