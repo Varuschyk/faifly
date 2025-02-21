3ef7488104facf3460689532ac192834b9a114b9
O.Varushchyk <alexvarushik@gmail.com>
2025-02-21 11:10:56 +0200

    Remove PatientService.java

--- faifly-api-core/src/main/java/com/task/faiflyapicore/service/PatientService.java
+++
@@ -1,9 +0,0 @@
-package com.task.faiflyapicore.service;
-
-import com.task.faiflyapicore.pojo.patient.PatientReadPojo;
-import jakarta.annotation.Nonnull;
-
-public interface PatientService {
-
-	PatientReadPojo get(@Nonnull Long id);
-}
