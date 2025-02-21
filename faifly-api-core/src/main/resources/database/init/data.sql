7e29a9c7de5681b54de633358477aa6a32ea5b1e
O.Varushchyk <alexvarushik@gmail.com>
2025-02-21 14:46:47 +0200

    Add additional data for initialization db

--- faifly-api-core/src/main/resources/database/init/data.sql
+++ faifly-api-core/src/main/resources/database/init/data.sql
@@ -1,5 +1,17 @@
 INSERT INTO DOCTORS (id, first_name, last_name, time_zone)
 VALUES (1, 'Pete', 'Anderson', 'UTC');
+INSERT INTO DOCTORS (id, first_name, last_name, time_zone)
+VALUES (2, 'Anna', 'Karpenko', 'UTC');
+INSERT INTO DOCTORS (id, first_name, last_name, time_zone)
+VALUES (3, 'Kate', 'Zueva', 'UTC');
 
 INSERT INTO PATIENTS (id, first_name, last_name)
 VALUES (1, 'Angelo', 'Herringhton');
+INSERT INTO PATIENTS (id, first_name, last_name)
+VALUES (2, 'Alexander', 'Var');
+INSERT INTO PATIENTS (id, first_name, last_name)
+VALUES (3, 'Michel', 'Shevchenko');
+INSERT INTO PATIENTS (id, first_name, last_name)
+VALUES (4, 'Peter', 'Avramenko');
+INSERT INTO PATIENTS (id, first_name, last_name)
+VALUES (5, 'Karl', 'Melnyk');
